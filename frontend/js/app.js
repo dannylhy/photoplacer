cc7fbf37db1066632b7302dc17eca2af52155c52
OrangePhoenix <ogaponenko3@gmail.com>
2013-10-29 17:00:29 -0400

    More Reorganization
    Rewrote app to work as a one-webpage app. photo.html and wishlist.html
    currently do not serve a purpose.
    
    Implemented functionality for switching between main map, wishlist, and
    list of photos of a location. Still some issues (wishlist API doesn't
    seem to respond).

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,11 +1,47 @@
 /* ON READY */
 
-$(document).ready(function initialize() {      
+$(document).ready(function initialize() {
+
+	$("#wishLink").click(function() {
+		console.log("WishList clicked");
+		
+		$("#map-canvas").css('display', 'none');
+		$("#photoList").css('display', 'none');
+		$("#wishList").css('display', 'inline');
+		
+		// Retrieve a list of photos for the given marker
+		$.ajax({
+			url: "http://dev.m.gatech.edu/d/dlee399/w/photoplacer/c/api/wishlist",
+			context: document.body,
+			success: function(data) {
+				console.log("wishList index AJAX success.");
+				
+				//var photos = jQuery.parseJSON(data);
+				var photos = data;
+			
+				/*
+				document.write("<div id = \"wishList\">" +
+					"<ul style=\"list-style: none;\">");
+				
+				for (var i = 0; i < photos.length; i++) {
+					var url = photos[i].url;
+					document.write("<li>" +				
+						"<a href=\"" + url + "\">" +
+						"<img src=\"" + url + "\" width=\"150\" height=\"150\"></a>" +
+						"</li>");
+				}
+				
+				document.write("</ul>");
+				*/
+				
+				//$("#map-canvas").css('display', 'none');
+			}
+		});
+		
+		$("#wishList").css('display', 'inline');
+	});
+
 	initializeMap();
-	
-	$("#photoList").append("<p>Test</p>");
-	
-	$("#photoList").append("<p>Test</p>");
 });
 
 /* GLOBALS */
@@ -14,10 +50,6 @@ var Map; // Reference to the Google API Map
 var Markers = []; // Markers being processed
 var PrevMarkerID = 0; // Marker ID for the previous zoom level
 var Level = 0; // Zoom level
-
-// Managing marker photos display
-var IWMapMarkerID = 0;
-var IWMapZoomMarkerID = 0;
 
 /* FUNCTIONS */
 
@@ -125,7 +157,7 @@ function popOutPhoto(dataMarker, mapMarker) {
 			var firstPhoto = parsedData[0];
 			
 			var popPhotoString = '<div>' +
-			'<a href="photo.html"> <img src="' + firstPhoto.url + '" width="150" height="150"> </a>' +
+			'<a href="#"> <img src="' + firstPhoto.url + '" width="150" height="150"> </a>' +
 			'</div>';
 		
 			var popPhotoIW = new google.maps.InfoWindow({
@@ -134,40 +166,53 @@ function popOutPhoto(dataMarker, mapMarker) {
 			
 			google.maps.event.addListener(mapMarker, 'click', function() {				
 				console.log("popPhotoIW marker event.");
-				
-				//IWMapMarkerID = 0;
-				IWMapZoomMarkerID = dataMarker.id;
-				
-				$("#PhotoListPhotos").append("<li>" +
-					"<a href=\"http://i.imgur.com/iZVfXgJ.jpg\">" +
-					"<img src=\"http://i.imgur.com/iZVfXgJ.jpg\" width=\"150\" height=\"150\">" +
-					"</a> </li>");
-					
-				$("#PhotoListPhotos").append("<p>Test</p>");
-				
-				$("#photoList").empty();
-				$("#photoList").append("<p>Test</p>");
+		
+				// Retrieve a list of photos for the given marker
+				$.ajax({
+					url: "http://dev.m.gatech.edu/d/dlee399/w/photoplacer/c/api/map/" + PrevMarkerID + "/zoom/" + dataMarker.id,
+					context: document.body,
+					success: function(data) {
+						console.log("getPopularPhotos() AJAX success.");
+						console.log("API url: " + "http://dev.m.gatech.edu/d/dlee399/w/photoplacer/c/api/map/" + PrevMarkerID + "/zoom");
+						
+						//var photos = jQuery.parseJSON(data);
+						var photos = data;
+						
+						console.log(photos);
+						
+						$("#map-canvas").css('display', 'none');
+						$("#photoList").css('display', 'inline');
+						
+						for (var i = 0; i < photos.length; i++) {
+							var url = photos[i].url;
+							
+							$("#PhotoListPhotos").append( 
+								"<li>" +				
+								"<a href=\"" + url + "\">" +
+								"<img src=\"" + url + "\" width=\"150\" height=\"150\"></a>" +
+								"</li>"
+							);
+						}
+
+						/*
+						document.write("<div id = \"photoList\">" +
+							"<ul style=\"list-style: none;\">");
+						
+						for (var i = 0; i < photos.length; i++) {
+							var url = photos[i].url;
+							document.write("<li>" +				
+								"<a href=\"" + url + "\">" +
+								"<img src=\"" + url + "\" width=\"150\" height=\"150\"></a>" +
+								"</li>");
+						}
+						
+						document.write("</ul>");
+						*/
+					}
+				});
 				
 				popPhotoIW.open(Map, mapMarker);
 			});
-		}
-	});
-}
-
-function getPopularPhotos(id) {
-	$.ajax({
-		url: "http://dev.m.gatech.edu/d/dlee399/w/photoplacer/c/api/map/" + PrevMarkerID + "/zoom/" +id,
-		context: document.body,
-		success: function(data) {
-			console.log("getPopularPhotos() AJAX success.");
-            //var photos = jQuery.parseJSON(data);
-			var photos = data;
-
-			window.location = "photo.html" ;
-			for (i = 0; i < photos.length; i++){
-			document.write("<img src=\"" + photos[i].url + "\"> <p></p>");
-			//$("#photo_list").html("<img src=\"" + photos[i].url + "\"> <p>hello test text</p>");
-			}
 		}
 	});
 }
